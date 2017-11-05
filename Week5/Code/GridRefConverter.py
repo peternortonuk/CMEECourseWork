f785fca1b7eabc70c0739dc98e92fe56a31c77a8
Peter Norton <peter.norton.uk@gmail.com>
2017-11-05 17:14:11 +0000

    generate result for every row in df

--- Week5/Code/GridRefConverter.py
+++ Week5/Code/GridRefConverter.py
@@ -5,48 +5,65 @@
 __author__ = "Petra Guy, pg5117@ic.ac.uk"
 __version__ = "2.7"
 
+import pandas as pd
+import math
+
 
 def getOSGridReference(e, n):
-	import math
-	
-	# The charachters for the start of grid ref
-	gridChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ"  	
-	
-	# get the 100km-grid indices, so need 6 figs, add 0s if required
-	e100k = math.floor(e/100000)
-	n100k = math.floor(n/100000)
-	
-	
-	# translate those into numeric equivalents 
-        # of the grid letters
-	l1 = (19-n100k)-(19-n100k)%5+math.floor((e100k+10)/5)
-	l2 = (19-n100k)*5%25 + e100k%5
-	
-	letPair = gridChars[int(l1)] + gridChars[int(l2)]
-	
-	# strip 100km-grid indices from easting & northing,
-        # round to 100m
-	e100m = math.trunc(round(float(e)/100))
-	egr = str(e100m).rjust(4, "0")[1:] 
-	if n >= 1000000:
-		n = n - 1000000 # Fix Shetland northings
-	n100m = math.trunc(round(float(n)/100))
-	ngr = str(n100m).rjust(4, "0")[1:]
-	
-	return letPair + egr + ngr
+
+    # The characters for the start of grid ref
+    gridChars = "ABCDEFGHJKLMNOPQRSTUVWXYZ"
+
+    # get the 100km-grid indices, so need 6 figs, add 0s if required
+    e100k = math.floor(e / 100000)
+    n100k = math.floor(n / 100000)
+
+    # translate those into numeric equivalents
+    # of the grid letters
+    l1 = (19 - n100k) - (19 - n100k) % 5 + math.floor((e100k + 10) / 5)
+    l2 = (19 - n100k) * 5 % 25 + e100k % 5
+
+    letPair = gridChars[int(l1)] + gridChars[int(l2)]
+
+    # strip 100km-grid indices from easting & northing,
+    # round to 100m
+    e100m = math.trunc(round(float(e) / 100))
+    egr = str(e100m).rjust(4, "0")[1:]
+    if n >= 1000000:
+        n = n - 1000000  # Fix Shetland northings
+    n100m = math.trunc(round(float(n) / 100))
+    ngr = str(n100m).rjust(4, "0")[1:]
+
+    return letPair + egr + ngr
 
 
 # test
-print getOSGridReference(96000,906000) # NA960060
-print getOSGridReference(465149, 1214051) # HP651141
+assert getOSGridReference(96000, 906000) == 'NA960060'
+assert getOSGridReference(465149, 1214051) == 'HP651141'
 
-# put my easting and nortings in...
-import pandas as pd
+# import the file
+woodLocs = pd.read_csv(r'C:\dev\code\petra\Week5\Data\Area_Site.csv')
 
-woodLocs = pd.read_csv('../Data/Area_Site.csv')
-print woodLocs
-woodLocs['Easting'] = woodLocs['Easting']*100
-woodLocs['Northing']= woodLocs['Northing']*100
+# add easting and northings; why by a factor of x100?
+woodLocs['Easting'] = woodLocs['Easting'] * 100
+woodLocs['Northing'] = woodLocs['Northing'] * 100
 
 woodLocsOS = woodLocs
-woodLocsOS['OS'] = getOSGridReference(woodLocsOS.Easting,woodLocs.Northing)
+
+# three different ways to modify
+# (1) use a lambda function
+# woodLocsOS['OS'] = woodLocsOS.apply(lambda x: getOSGridReference(x['Easting'], x['Northing']), axis=1)
+
+# (2) sledgehammer
+# x = [getOSGridReference(woodLocsOS.Easting.iloc[i], woodLocsOS.Northing.iloc[i]) for i in range(len(woodLocsOS.Easting))]
+# woodLocsOS['OS'] = x
+
+# (3) pandas row iterator
+# You should never modify something you are iterating over; so create a new list
+x = []
+for index, row in woodLocsOS.iterrows():
+    x.append(getOSGridReference(row['Easting'], row['Northing']))
+woodLocsOS['OS'] = x
+
+print woodLocsOS.head()
+
