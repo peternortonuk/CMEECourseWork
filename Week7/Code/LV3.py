bf527475bf8bd0a7d98276aec0d920072259eee7
PetraGuy <petraguy99@yahoo.com.au>
2017-11-13 17:38:31 +0000

    LV£ update

--- Week7/Code/LV3.py
+++ Week7/Code/LV3.py
@@ -15,26 +15,27 @@ import pandas as pd
 
 
 columns = ['R','C']
-results = pd.DataFrame()
-
+results = pd.DataFrame({'R':y0, 'C':x0}, index=[0])
 def dR_dt(z0, r,a,z,e):
     """ Returns the growth rate of predator and prey populations at any
     given time step """
-    R = z0[0]
-    C = z0[1]
-    for t in range(1,1000):
-        Rnew = R + R*r -R*R*r - a*C
-        Cnew = C - C*z+C*e*a*R
-        R = R+t
-        C = C+t
-        df = pd.DataFrame({'R':Rnew, 'C':Cnew})
-        results = results.append(df, ignore_index=True,)
+    #results = results.append(z0, index)
+    for t in range(0,1000):
+        t=0
+        Rt = results[t][1]
+        Ct = results[t][1]
+        R = Rt + Rt*r -Rt*Rt*r - a*Ct
+        C = Ct - Ct*z+Ct*e*a*Rt
+        df = pd.DataFrame({'R':Rt, 'C':Ct}, index=[0])
+        print df
+        results = results.append(df, ignore_index=True)
     return results
 
 def main(argv):
     x0 = 10
     y0 = 5
     z0 = sc.array([x0, y0])
+    z0 = pd.DataFrame(data = z0)
     r = 1.  # Resource growth rate
     a = 0.1  # Consumer search rate (determines consumption rate)
     z = 1.5  # Consumer mortality rate
