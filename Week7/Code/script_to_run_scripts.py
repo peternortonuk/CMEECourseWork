6cfd19cf3afe74112177eca62d0ceed8b23a05dd
PetraGuy <petraguy99@yahoo.com.au>
2017-11-15 14:26:53 +0000

    working on subprocces

---
+++ Week7/Code/script_to_run_scripts.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python
+
+import subprocess
+
+#subprocess.Popen("python LV1.py &", shell=True).wait()
+subprocess.Popen("python LV2.py", "0.1 0.1 0.5 0.1").wait()
---
+++ Week7/Sandbox/bash_to_run_Week7.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#bash script to run python and R scripts
+
+cd ../Code
+python -p LV2.py 0.1 0.1 0.1 0.1
+
