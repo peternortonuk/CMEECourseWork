165921abc760e925fa10185d483d0869a2253369
PetraGuy <petraguy99@yahoo.com.au>
2018-02-01 22:17:06 +0000

    DBH finished mean

---
+++ CMEEMainProject/Sandbox/test_script.R
@@ -0,0 +1,78 @@
+
+
+means = data.frame()
+dbh_classes = c(1:32)
+dbh_values = seq(from = 7.5, to = 162.5, length = 32)
+for (i in 1:103){
+  sitedata = DBH_Yr2_agg%>%filter(SITE == i)
+  for (j in 1:16) {
+    plotdata = sitedata%>%filter(PLOT == j)
+    means[i,j] = round(sum(plotdata$Count*(plotdata$DBH_class*5+2.5))/sum(plotdata$Count), digits = 2)
+    }
+}
+rownames(means) = c(1:103)
+colnames(means) = c(1:16)
+columns = as.character(c(1:16))
+mean_10 = subset(means, columns < 10)
+
+for (i in 1:103){
+  for (j in 1:16){
+    if (means[i,j] < 10){
+    Site = paste("Site",i, sep = "")
+    Plot = paste("Plot",j, sep = "")
+    print(paste(Site, Plot, sep = " " ))
+    print(means[i,j])
+  }
+}
+}
+threshold = 10
+GetSites = function(lower, upper){
+  m_df = data.frame()
+  for (i in 1:103){
+    for (j in 1:16){
+      if( (means[i,j] < upper) && (means[i,j] > lower) ){
+        m_df = rbind(m_df,c(i,j,means[i,j]))
+      }}}
+  colnames(m_df) = c("Site", "Plot","Mean")
+  
+  plot_list = list()
+ 
+  if(nrow(m_df) >= 4){
+    for (i in 1:4){
+    #browser()
+    rand = sample(nrow(m_df),4)
+    tmp = rand[1]
+    rand_row = m_df[tmp,]
+    site = rand_row[[1]]
+    plot = rand_row[[2]]
+    mu = rand_row[[3]]
+    
+  data = DBH_Yr2_agg%>%filter(SITE==site)%>%filter(PLOT==plot)
+  Plot = ggplot(data = data, aes(x = DBH_class, y = Count)) +
+    geom_bar(stat = "identity", col = "black", fill = "green") +
+    scale_x_continuous("DBH class")+
+    labs(title = mu)
+  plot_list[[i]] = Plot
+    }
+    grid.arrange(grobs = plot_list, ncol = 2)
+  }
+}
+
+
+data = DBH_Yr2_agg%>%filter(SITE==78)%>%filter(PLOT==2 )
+Plot = ggplot(data = data, aes(x = DBH_class, y = Count)) +
+  geom_bar(stat = "identity", position="dodge", width = 0.5,col = "black", fill = "green") +
+  scale_x_continuous("DBH class", 
+                     breaks = 1:15,
+                     labels = c(1:15),
+                     limits = c(0, 15))+
+  labs(title = 7.98)
+Plot
+
+
+
+
+
+
+
+
